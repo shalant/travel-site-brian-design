d4990c763e14e6ffd4225d4cd55d0393a01994a4
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-03-31 17:26:37 -0500

    very nice, almost done with styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -25,12 +25,86 @@ function Footer() {
                     <div className='footer-link-items'>
                         <h2>About Us</h2>
                         <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                    <div className='footer-link-items'>
+                        <h2>About Us</h2>
                         <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                </div>
+                <div className='footer-link-wrapper'>
+                    <div className='footer-link-items'>
+                        <h2>About Us</h2>
                         <Link to='/sign-up'>How it works</Link>
-
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                    <div className='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
                     </div>
                 </div>
             </div>
+            <section className='social-media'>
+                <div className='social-media-wrap'>
+                    <div className='footer-logo'>
+                        <Link to='/' className='social-logo'>
+                            TRVL <i className='fab fa-typo3' />
+                        </Link>
+                    </div>
+                    <small className='website-rights'>TRVL @2021</small>
+                    <div className='social-icons'>
+                        <Link className='social-icon-link facebook'
+                            to='/'
+                            target='_blank'
+                            aria-label='Facebook'
+                        >
+                            <i className='fab fa-facebook-f'></i>
+                        </Link>
+                        <Link className='social-icon-link instagram'
+                            to='/'
+                            target='_blank'
+                            aria-label='Instagram'
+                        >
+                            <i className='fab fa-instagram'></i>
+                        </Link>
+                        <Link className='social-icon-link youtube'
+                            to='/'
+                            target='_blank'
+                            aria-label='Youtube'
+                        >
+                            <i className='fab fa-youtube'></i>
+                        </Link>
+                        <Link className='social-icon-link twitter'
+                            to='/'
+                            target='_blank'
+                            aria-label='Twitter'
+                        >
+                            <i className='fab fa-twitter'></i>
+                        </Link>
+                        <Link className='social-icon-link linkedin'
+                            to='/'
+                            target='_blank'
+                            aria-label='LinkedIn'
+                        >
+                            <i className='fab fa-linkedin'></i>
+                        </Link>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
