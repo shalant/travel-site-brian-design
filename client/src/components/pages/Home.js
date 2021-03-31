413883945a8b1ded72970b2d1bee77640608921f
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-03-31 15:56:31 -0500

    mo

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,12 +2,14 @@ import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
 import React from 'react'
+import Footer from '../Footer';
 
 function Home() {
     return (
         <>
             <HeroSection />
             <Cards />
+            <Footer />
         </>
     )
 }
