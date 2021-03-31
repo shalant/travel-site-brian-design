771affc46e79476fe8dd1e1a3f1b26b72ff28e15
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-03-31 12:51:31 -0500

    making great progress, nice walkthrough

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import Cards from '../Cards';
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+        </>
+    )
+}
+
+export default Home
